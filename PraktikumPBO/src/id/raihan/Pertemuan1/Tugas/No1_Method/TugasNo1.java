c075e44906858ea2aa7b950bb2222e22ed2c47ef
raihaniqbalpasya <raihaniqbalpasya.hsb@gmail.com>
2020-10-23 21:57:26 +0700

    Praktikum dan Tugas Pertemuan 1

---
+++ PraktikumPBO/src/id/raihan/Pertemuan1/Tugas/No1_Method/TugasNo1.java
@@ -0,0 +1,21 @@
+package id.raihan.Pertemuan1.Tugas.No1_Method;
+
+// class untuk mengonversi suhu
+public class TugasNo1 {
+    // method-method konversi suhu
+    public double cf(double suhu) {
+        return (suhu * 1.8 + 32);
+    }
+
+    public double fk(double suhu) {
+        return ((suhu + 459.67) / 1.8);
+    }
+
+    public double kr(double suhu) {
+        return ((suhu - 273.15) * 0.8);
+    }
+
+    public double rc(double suhu) {
+        return (suhu * 1.25);
+    }
+}
